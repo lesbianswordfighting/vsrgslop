402f69a3ebaacdba89830e67a24cc38a5e5ea8f7
lesbianswordfighting <personallymara@gmail.com>
2025-11-09 22:06:02 -0800

    basic input implementation
    added a check for when the note is in the receptor - only works for individual input, figuring out how to get it to work for all keys

--- scripts/perfect_window.gd
+++ scripts/perfect_window.gd
@@ -1,1 +1,19 @@
 extends Area2D
+
+@export var key_name: String = ""
+
+#checking for if notecollision is in perfect_window
+func _ready() -> void:
+	monitoring = true
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed(key_name):
+		#check if notecollision is in perfect_window
+		var overlapping_bodies = get_overlapping_bodies()
+		
+		if overlapping_bodies.size() > 0:
+			print("overlapping with:", overlapping_bodies)
+		else:
+			print("not overlapping with anything")
+			
+	
